9c42185b206e1d5d66d048a93ce1f35b67427e0c
lariel_Santos <lariel_Santos@W106LNP2J2.poa.amer.dell.com>
2017-05-24 17:41:12 -0300

    inicial

---
+++ src/Maquina.java
@@ -0,0 +1,33 @@
+
+public class Maquina {
+	private String tag, modelo;
+
+	public Maquina(String tag, String modelo) {
+		super();
+		this.tag = tag;
+		this.modelo = modelo;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	@Override
+	public String toString() {
+		return "Maquina tag=" + tag + ", modelo=" + modelo;
+	}
+	
+	
+}
---
+++ src/Validar.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Validar {
+	private ArrayList<Maquina> maquinas;
+	
+	public Validar(){
+		maquinas=new ArrayList<Maquina>();
+		
+	}
+	
+	public boolean add(Maquina tag){
+		return maquinas.add(tag);
+	}
+	
+}
+//http://quality.dell.com/Search?tag=55VP2K2
+
