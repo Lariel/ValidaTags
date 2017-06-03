4706315b4887df37461a0e07771c7b62d20dff86
lariel_Santos <lariel_Santos@W106LNP2J2.poa.amer.dell.com>
2017-06-03 16:15:45 -0300

    funcional v1 (apenas rede interna)

--- src/business/Validar.java
+++ src/business/Validar.java
@@ -1,7 +1,14 @@
 package business;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -9,23 +16,62 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Validar {
-	private ArrayList<Maquina> maquinas;
+	//private ArrayList<String> tags;
+	private ArrayList<Maquina> modelos;
 	
 	public Validar(){
-		maquinas=new ArrayList<Maquina>();
+		//tags=new ArrayList<String>();
+		modelos=new ArrayList<Maquina>();
+	}
+	
+	public boolean add(Maquina m){
+		return modelos.add(m);
+	}
+	
+	public ArrayList<Maquina> validar(String tags){
 		
+		Scanner sc = new Scanner(tags).useDelimiter("\n"); 
+		
+		while (sc.hasNext()) {
+			String tag=sc.next();
+			String modelo="";
+			
+			String stringURL = "http://quality.dell.com/Search?tag="+tag;
+	        String consultaQuality = "";
+	        try {
+	            URL url = new URL(stringURL);
+	            URLConnection connection = url.openConnection();
+	            BufferedReader in = new BufferedReader(
+	                    new InputStreamReader(
+	                    connection.getInputStream()));
+	            String inputLine;
+	            StringBuffer sb = new StringBuffer();
+	            while ((inputLine = in.readLine()) != null) sb.append(inputLine);
+	            consultaQuality = sb.toString();
+	            in.close();
+	        } catch (MalformedURLException ex) {
+	            ex.printStackTrace();
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	        }
+	        
+	        if(consultaQuality.contains("Wyse")){
+	        	modelo = "wyse";
+	        }else modelo="outros";
+	        
+	        Maquina m=new Maquina(tag,modelo);
+			modelos.add(m);
+			
+			
+		}
+
+		sc.close();
+
+		return modelos;
 	}
-	
-	public boolean add(Maquina tag){
-		return maquinas.add(tag);
-	}
-	
-	
-	
-	
-	
 }
 //http://quality.dell.com/Search?tag=55VP2K2
 
--- src/ui/TelaController.java
+++ src/ui/TelaController.java
@@ -1,11 +1,44 @@
 package ui;
 
+import java.util.ArrayList;
+
+import business.Maquina;
+import business.Validar;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 
 public class TelaController {
+	
+	Validar validador=new Validar();
+	private ArrayList<Maquina> modelos; 
+	
+	ObservableList<Maquina> listaTags= FXCollections.observableArrayList(); 
 
     @FXML
-    private ListView<String> lvTags;
+    private TextArea taTags;
+
+    @FXML
+    private ListView<Maquina> lvModelos;
+    
+    @FXML
+    private Button btValidar;
+    
+    @FXML
+    void ValidarTags(ActionEvent event) {
+    	modelos=validador.validar(taTags.getText().toString());
+    	//System.out.println("aqui"+taTags.getText());
+    	for(int i=0;i<modelos.size();i++){
+    		listaTags.add(modelos.get(i));
+    	}
+    	lvModelos.setItems(listaTags);
+    	
+    }
+    
+    
 
 }
